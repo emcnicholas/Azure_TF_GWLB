aa149036e844d2b24661c6b5dd3e642827f7736e
emcnicholas <emcnicholas@comast.net>
2024-02-07 12:36:01 -0500

    updated variables

--- variable.tf
+++ variable.tf
@@ -3,31 +3,6 @@ variable "location" {
   default = "eastus"
 }
 
-## Number of zones for www
-#variable "www_zones" {
-#  default = 2
-#}
-#
-## Number of www VMs per zone
-#variable "www_per_zone" {
-#  default = 2
-#}
-#
-## Number of zones for FW
-#variable "fw_zones" {
-#  default = 2
-#}
-#
-## Number of FWs per zone
-#variable "fw_per_zone" {
-#  default = 1
-#}
-
-# Cluster Prefix
-#variable "cluster_prefix" {
-#  default = "ftd-cluster"
-#}
-
 # Source IP address where ssh connection to the bastian host would initiate.
 variable "ssh_sources" {
   default = ["173.75.255.105/32"]
